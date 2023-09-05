fa2fe6b7922b2c7af65034db45aae78fefd5ba51
taufanp <taufanprihantoro15@gmail.com>
2023-09-05 17:20:54 +0700

    Fix image local source

--- src/components/atoms/Picture/index.tsx
+++ src/components/atoms/Picture/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Image, ImageProps} from 'react-native';
 import {ImagePlaceholder} from '../../../assets/images';
 import testId from '../../../constants/testId';
+import {DUMMY_IMAGE_LOCAL} from '../../../constants/components';
 
 interface PictureProps extends Omit<ImageProps, 'source'> {
   uri?: string;
@@ -11,7 +12,7 @@ function Picture({uri = ImagePlaceholder, ...props}: PictureProps) {
   return (
     <Image
       testID={testId.PICTURE_ATOM}
-      defaultSource={require(ImagePlaceholder)}
+      defaultSource={DUMMY_IMAGE_LOCAL}
       {...props}
       source={{uri}}
     />
--- src/constants/components/index.ts
+++ src/constants/components/index.ts
@@ -1,1 +1,2 @@
 export const DUMMY_IMAGE_URL = 'https://picsum.photos/200';
+export const DUMMY_IMAGE_LOCAL = require('../../assets/images/image-placeholder.jpg');
